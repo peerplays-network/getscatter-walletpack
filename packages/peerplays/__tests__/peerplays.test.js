3ca8bf49a9eee1e4b2a9b7bd1cd4178522cd2a4b
mseaward <m.seaward@pbsa.info>
2020-02-05 11:55:43 -0400

    Corrected testing account creds and added all it's pub keys

--- packages/peerplays/__tests__/peerplays.test.js
+++ packages/peerplays/__tests__/peerplays.test.js
@@ -5,8 +5,27 @@ require('isomorphic-fetch');
 const peerplays = new (require('../lib/peerplays').default)();
 const RandomString = require('randomstring');
 
-const username = 'ikPdC6fahHQc';
-const password = 'jdg0OmtkzRIWfTMpN8jNZ1cbPMzGCG4hQmsDIQYfEzTEthJN4d5x';
+const username = 'unit39';
+const password = 'A4qVtjUa8t6x4mZz7BQoDWWF8Qp6UEFXtrlrBN5BPxWChenbdwKm';
+const activeWIF = '5KeU4uAV8YT4kZazX4hqakdsPck2L22dZYCHxMFkZL5TsRYex6j'
+const pubKeys = [
+  'PPY4yaEWqYHxR8QxrFFFpVwYSgzNaMUVmiNgCwM31WPbjNPDUrKqn', // owner
+  'PPY8QGhjBytYZrHpmDorLM4ETsoDYXGbGH3WT8sTrhu3LUJQ9ePf5', // active
+  'PPY64uMTGiYZQkn5P7dy87fnfVz68b4HYEBRTK1bc3wqEVTcp2GtB'  // memo
+];
+const roles = ['owner','active','memo'];
+
+// account keys for 'unit39'
+const KEYPAIR = Keypair.fromJson({
+  privateKey: activeWIF,
+  blockchains: [Blockchains.PPY],
+  publicKeys: [{
+    key: 'PPY8QGhjBytYZrHpmDorLM4ETsoDYXGbGH3WT8sTrhu3LUJQ9ePf5', // active
+    // ownerKey: 'PPY4yaEWqYHxR8QxrFFFpVwYSgzNaMUVmiNgCwM31WPbjNPDUrKqn', // owner
+    // memoKey: 'PPY64uMTGiYZQkn5P7dy87fnfVz68b4HYEBRTK1bc3wqEVTcp2GtB', // memo
+    blockchain: Blockchains.PPY
+  }]
+});
 
 describe('peerplays', () => {
   it('should be able to retrieve a Peerplays accounts keys', async () => {
